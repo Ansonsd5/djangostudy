dde87ea3bcfa7d3ecc08ba8b52b611497c56e60f
Ansonsd5 <anson11sd@gmail.com>
2022-05-06 20:46:13 +0530

    rendering data from models

--- base/views.py
+++ base/views.py
@@ -1,21 +1,20 @@
-from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-rooms=[
-    {'id':1,'name':'Lets learn Technical Analysis'},
-    {'id':2,'name':'lets learn baics of Options'},
-    {'id':3,'name':'Basics of Swing Trading'},
-]
+# rooms=[
+#     {'id':1,'name':'Lets learn Technical Analysis'},
+#     {'id':2,'name':'lets learn baics of Options'},
+#     {'id':3,'name':'Basics of Swing Trading'},
+# ]
 
 def home(request):
+    rooms =Room.objects.all()
     context={'rooms':rooms}
     return render(request, 'base/home.html',context)
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room=i
-        context={'room':room}
+    room = Room.objects.get(id=pk)
+    context={'room':room}
     return render(request,'base/room.html',context)
+
