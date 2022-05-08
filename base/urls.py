4d1ff4b40ecc4b035fd6f44a1aa5891acd704f62
Ansonsd5 <anson11sd@gmail.com>
2022-05-08 21:04:06 +0530

    updated with form

--- base/urls.py
+++ base/urls.py
@@ -9,4 +9,5 @@ from django.http import HttpResponse
 urlpatterns = [
     path('',views.home, name="home"),
     path('room/<str:pk>/',views.room,name="room"),
+    path('create-room/', views.createRoom, name="create-room"),
 ]
