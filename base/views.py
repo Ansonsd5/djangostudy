64871ff6e90be61db502f9bc3e4c958f1988e9e3
Ansonsd5 <anson11sd@gmail.com>
2022-05-10 00:13:00 +0530

    fix - with all the CRUD operations

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Room
 from .forms import RoomForm
@@ -22,6 +23,31 @@ def room(request,pk):
 def createRoom(request):
     form = RoomForm()
     if request.method == "POST":
-        print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
     context ={'form':form}
     return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form =RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+
+    context={'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    return render(request, 'base/delete.html', {'obj':room})
