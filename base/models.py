bab74dfebd5b62c37831ce13af548ceb7d6570cc
Ansonsd5 <anson11sd@gmail.com>
2022-06-03 21:32:30 +0530

    feature: imported the theme and done with setup

--- base/models.py
+++ base/models.py
@@ -13,12 +13,13 @@ class Room(models.Model):
     host = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
     topic =  models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
+    participants =models.ManyToManyField(User,related_name='participants',blank=True)
     description = models.TextField(null=True, blank=True)
     updated = models.TimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
     class Meta:
-        ordering =['-updated','created']
+        ordering =['-updated','-created']
 
     def __str__(self):
         return self.name
@@ -30,5 +31,10 @@ class Message(models.Model):
     updated = models.TimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering =['-updated','-created']
+
     def __str__(self):
         return self.body[0:50]
+
+    
--- base/views.py
+++ base/views.py
@@ -1,4 +1,6 @@
+from email import message
 from multiprocessing import context
+from pydoc_data.topics import topics
 
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
@@ -6,7 +8,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login,logout
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
-from .models import Room, Topic
+from .models import Message, Room, Topic
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RoomForm
 from django.contrib import messages
@@ -86,14 +88,34 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context={'rooms':rooms, 'topics':topics, 'room_count':room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    context={'rooms':rooms, 'topics':topics, 'room_count':room_count,'room_messages':room_messages}
     return render(request, 'base/home.html',context)
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
-    context={'room':room, 'room_messages': room_messages}
+    room_messages = room.message_set.all()
+    participants =room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user =request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room',pk=room.id)
+
+
+    context={'room':room, 'room_messages': room_messages, 'participants':participants}
     return render(request,'base/room.html',context)
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms= user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms,'room_messages': room_messages,'topics':topics}
+    return render(request, 'base/profile.html', context)
 
 @login_required(login_url='login')
 
@@ -102,7 +124,9 @@ def createRoom(request):
     if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host=request.user
+            room.save()
             return redirect('home')
 
     context ={'form':form}
@@ -134,3 +158,15 @@ def deleteRoom(request, pk):
         return redirect('home')
 
     return render(request, 'base/delete.html', {'obj':room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':message})
